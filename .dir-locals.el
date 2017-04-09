c77d0fd280d16f00aa951ec36676351fd9d97e0c
Michal Rus <m@michalrus.com>
2017-04-09 03:51:31 +0200

    Add the old, naïve /bin/sh shim

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((eval . (setq-local intero-stack-executable
+                             (expand-file-name
+                              ".intero-shim.sh"
+                              (locate-dominating-file default-directory ".dir-locals.el")))))))
