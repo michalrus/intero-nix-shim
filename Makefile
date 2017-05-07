72677fb3a9f475f7f53715b18dc251ed83026410
Michal Rus <m@michalrus.com>
2017-05-07 02:05:05 +0200

    Move formatting check to the Makefile

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
-.PHONY: ci autoformat
+.PHONY: local ci build lint autoformat autoformat-check
 
-ci:
-	@nix-build
+local: autoformat       build lint
+ci:    autoformat-check build lint
 
+build:
+	@nix-build -j$$(nproc)
+lint:
+	@nix-shell --pure --run 'exec hlint .'
 autoformat:
-	@nix-shell --pure --run "exec $(MAKE) _autoformat"
+	@nix-shell --pure --run "exec $(MAKE) -j$$(nproc) _autoformat"
+autoformat-check: autoformat
+	@nix-shell --pure --run 'status=$$(git status --porcelain | grep -v "^M ") ; [ -z "$$status" ] || { printf >&2 "%s\n%s\n" "fatal: some files are unformatted (or repo unclean):" "$$status" ; exit 1 ; }'
 
 
 #———————————————————————————————————————————————————————————————————————————————
@@ -17,9 +23,6 @@ _autoformat: $(shell find . '(' -name '*.hs' -o -name '*.c' ')' -a -not -path '*
 dist/autoformat/%_fmt: %
 	@echo "Formatting $<..."
 	@case "$<" in \
-			*.hs) hlint "$<" \
-				 && hindent --line-length 80 "$<" \
-				 && stylish-haskell --inplace "$<" \
-						;; \
-			*.c) indent -linux "$<" && rm "$<"~ ;; \
-		esac && mkdir -p "$(dir $@)" && touch "$@"
+		*.hs) hindent --line-length 80 "$<" && stylish-haskell --inplace "$<" ;; \
+		*.c) indent -linux "$<" && rm "$<"~ ;; \
+	esac && mkdir -p "$(dir $@)" && touch "$@"
--- default.nix
+++ default.nix
@@ -4,22 +4,12 @@ with nixpkgs;
 
 let
 
-  fmtInputs = [ indent ] ++ (with haskellPackages; [ hlint hindent stylish-haskell ]);
-
   build = let # TODO: Consider using `cabal sdist`? https://git.io/vSo8l
     src = builtins.filterSource (path: type: # FIXME: How to re-use .gitignore? https://git.io/vSo80
          lib.all (i: toString i !=            path) [ ./.git ./dist ./result ]
       && lib.all (i:          i != baseNameOf path) [ ".stack-work" ])
       ./.;
   in lib.overrideDerivation (haskellPackages.callCabal2nix "intero-nix-shim" src {}) (oldAttrs: {
-    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ fmtInputs;
-    preBuild = ''
-      hashRaw=$(${nix}/bin/nix-hash --type sha256 .)
-      make _autoformat
-      rm -r dist/autoformat
-      hashFmt=$(${nix}/bin/nix-hash --type sha256 .)
-      [ "$hashRaw" = "$hashFmt" ] || { echo >&2 'fatal: a file was commited unformatted' ; exit 1 ; }
-    '';
     postInstall = ''
       mkdir -p $out/libexec
       ln -s ${haskellPackages.cabal-install}/bin/cabal  $out/libexec
@@ -28,7 +18,8 @@ let
   });
 
   env = lib.overrideDerivation build.env (oldAttrs: {
-    buildInputs = fmtInputs ++ (with haskellPackages; [
+    buildInputs = [ git indent ] ++ (with haskellPackages; [
+      hlint hindent stylish-haskell
       cabal-install intero # needed for ./dogfood.sh
     ]);
   });
